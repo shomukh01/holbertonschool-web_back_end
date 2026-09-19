97073646fe6711e263f71985f5aedb82bc305b7a
Shomukh <shomukh.aldosari@gmail.com>
2026-09-20 01:48:44 +0300

    Add type annotations to pagination helper

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,10 @@
 ﻿#!/usr/bin/env python3
 """Pagination helper functions."""
 
+from typing import Tuple
 
-def index_range(page, page_size):
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return the start and end indexes for a 1-indexed page."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
