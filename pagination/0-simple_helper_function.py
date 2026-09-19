4a48e7cb944e9981b7031e106292ded2e0862833
Shomukh <shomukh.aldosari@gmail.com>
2026-09-20 01:43:51 +0300

    Add pagination index helper

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env python3
+"""Pagination helper functions."""
+
+
+def index_range(page, page_size):
+    """Return the start and end indexes for a 1-indexed page."""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
